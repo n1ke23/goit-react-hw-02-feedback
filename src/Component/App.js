343690bead0feb07237ba327ab62874dac47dca9
Aleksandr Jashin <67903349+n1ke23@users.noreply.github.com>
2020-11-03 16:41:20 +0200

    add code, i think thet all

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import Buttons from './Buttons/Buttons'
+import Statistick from './Statistick/Statistick'
+import Section from './Section/Section'
+import Notification from './Notification/Notification'
+
 const state = {
   good: 0,
   neutral: 0,
@@ -11,19 +15,25 @@ function App() {
   const [states, setStates] = useState(state);
   // console.log(states);
   const addStep = (id) => {
-    console.log(id);
-    return setStates((el) => ({ ...el, [id]: el.[id] + 1 }))
+    // console.log(id);
+    return setStates((el) => ({ ...el, [id]: el[id] + 1 }))
   }
+
+  const countTotalFeedback = () => Object.values(states).reduce((acc, el) => acc + el, 0)
+
+  const countPositiveFeedbackPercentage = () => ((states.good / countTotalFeedback()) * 100).toFixed()
+  
   return (
     <>
-      <h1>Please leave feedback</h1>
-      <div>
-        {Object.keys(states).map((el) => <Buttons key={el} id={el} addStep={addStep} />)}
-      </div>
-      <h2>Statistick</h2>
-      <p>good: {state.good}</p>
-      <p>neutral: {state.neutral}</p>
-      <p>bad: {state.bad}</p>
+       <Section title={"Please leave feedback"}>
+          {Object.keys(states).map((el) => <Buttons key={el} id={el} addStep={addStep} />)}
+      </Section>
+      { countTotalFeedback() > 0 
+        ? (<Section titel={"Statistick"}>{Object.entries(states).map((el)=> <Statistick key={el[0]} name={el[0]} value={el[1]}/> )}<p>total: {countTotalFeedback()}</p><p>Positive feedback: {countPositiveFeedbackPercentage()}%</p></Section>) 
+        : (<Notification message="No feedback given" />)
+      }
+
+      
     </>
   );
 }
---
+++ src/Component/Statistick/Statistick.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Statistick = ({name, value}) => {
+  return (
+    <>
+      <p>{name}: {value}</p>
+    </>
+  );
+};
+
+export default Statistick;
